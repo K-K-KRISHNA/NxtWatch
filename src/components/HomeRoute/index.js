fd8be33559847bfeb00ceb4a4b6eb0e0f511ebdd
K-K-KRISHNA <kanakakrishnakeerthi@gmail.com>
2023-08-12 04:51:12 +0000

    After inserting Banner and fetching Data in hone Route 42 tests passed

--- src/components/HomeRoute/index.js
+++ src/components/HomeRoute/index.js
@@ -1,17 +1,105 @@
-// import {Component} from 'react'
+import Cookies from 'js-cookie'
+import {Component} from 'react'
+import {AiOutlineClose} from 'react-icons/ai'
 import Header from '../Header'
 import SideTabBar from '../SideTabBar'
-import {HomeContainer} from './styledComponents'
+
+import {
+  HomeContainer,
+  RemainContainer,
+  Logo,
+  PremiumSection,
+  Banner,
+  Tagline,
+  CrossButton,
+  GetitNowButton,
+} from './styledComponents'
 // import ThemeContext from '../../Context/ThemeContext'
 
-const HomeRoute = () => (
-  <>
-    <Header />
-    <HomeContainer>
-      <SideTabBar />
-      <h1>Home Route</h1>
-    </HomeContainer>
-  </>
-)
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+
+class HomeRoute extends Component {
+  state = {
+    isBanner: true,
+    apiStatus: apiStatusConstants.initial,
+    videos: [],
+  }
+
+  componentDidMount() {
+    this.getVideos()
+  }
+
+  getVideos = async () => {
+    this.setState({apiStatus: apiStatusConstants.inProgress})
+    const jwtToken = Cookies.get('jwt_token')
+    const apiUrl = 'https://apis.ccbp.in/videos/'
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+    const response = await fetch(apiUrl, options)
+    if (response.ok) {
+      const fetchedData = await response.json()
+      console.log(fetchedData)
+      this.setState({apiStatus: apiStatusConstants.success})
+    } else {
+      this.setState({apiStatus: apiStatusConstants.failure})
+    }
+  }
+
+  renderSuitableView = () => {
+    const {apiStatus} = this.state
+    switch (apiStatus) {
+      case apiStatusConstants.inProgress:
+        return <h1>Loading.....</h1>
+      case apiStatusConstants.success:
+        return <h1>Successfully Fetched data...</h1>
+      default:
+        return <h1>Failed to Fetched the Data..</h1>
+    }
+  }
+
+  closeBanner = () => this.setState({isBanner: false})
+
+  render() {
+    const {isBanner} = this.state
+
+    return (
+      <>
+        <Header />
+        <HomeContainer>
+          <SideTabBar />
+          <RemainContainer>
+            {isBanner && (
+              <Banner>
+                <PremiumSection>
+                  <Logo
+                    src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png"
+                    alt="website logo"
+                  />
+                  <Tagline>
+                    Buy Nxt Watch Premium prepaid plans with UPI
+                  </Tagline>
+                  <GetitNowButton>GET IT NOW</GetitNowButton>
+                </PremiumSection>
+                <CrossButton>
+                  <AiOutlineClose size="20" onClick={this.closeBanner} />
+                </CrossButton>
+              </Banner>
+            )}
+            {this.renderSuitableView()}
+          </RemainContainer>
+        </HomeContainer>
+      </>
+    )
+  }
+}
 
 export default HomeRoute
--- src/components/HomeRoute/styledComponents.js
+++ src/components/HomeRoute/styledComponents.js
@@ -4,3 +4,50 @@ export const HomeContainer = styled.div`
   display: flex;
   flex-direction: row;
 `
+export const RemainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 10px;
+`
+export const Banner = styled.div`
+  background-image: url('https://assets.ccbp.in/frontend/react-js/nxt-watch-banner-bg.png');
+  display: flex;
+  justify-content: space-between;
+  padding-left: 20px;
+  width: 100%;
+  height: 320px;
+  background-size: cover;
+`
+export const PremiumSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+`
+
+export const Logo = styled.img`
+  width: 30%;
+`
+export const Tagline = styled.p`
+  color: black;
+  font-size: 30px;
+`
+export const CrossButton = styled.button`
+  background-color: transparent;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  border: none;
+  cursor: pointer;
+`
+export const GetitNowButton = styled.button`
+  background-color: transparent;
+  height: 50px;
+  width: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  font-weight: bolder;
+  margin-top: 30px;
+`
